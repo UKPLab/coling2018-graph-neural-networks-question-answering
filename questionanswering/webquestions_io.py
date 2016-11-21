67a90b74ee8ec35b5acd9aaa9878108fceac2e85
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2016-11-21 17:36:41 +0100

    add tests to the webquestions io methods.

--- questionanswering/webquestions_io.py
+++ questionanswering/webquestions_io.py
@@ -7,5 +7,13 @@ def get_answers_from_question(question_object):
 
     :param question_object: A question encoded as a Json object
     :return: A list of answers as strings
+    >>> get_answers_from_question({"url": "http://www.freebase.com/view/en/natalie_portman", "targetValue": "(list (description \\"Padm\u00e9 Amidala\\"))", "utterance": "what character did natalie portman play in star wars?"})
+    ['Padmé Amidala']
+    >>> get_answers_from_question({"targetValue": "(list (description Abduction) (description Eclipse) (description \\"Valentine's Day\\") (description \\"New Moon\\"))"})
+    ['Abduction', 'Eclipse', "Valentine's Day", 'New Moon']
     """
     return re.findall("\(description \"?(.*?)\"?\)", question_object.get('targetValue'))
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
