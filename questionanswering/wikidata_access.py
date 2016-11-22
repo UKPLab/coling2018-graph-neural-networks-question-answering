650155f212816da9d97ae80391c7a6fe23dd0952
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2016-11-22 09:08:25 +0100

    Tests for entity mapping.

--- questionanswering/wikidata_access.py
+++ questionanswering/wikidata_access.py
@@ -114,7 +114,22 @@ def load_entity_map(path_to_map):
 entity_map = load_entity_map("../data/" + "entity_map.tsv")
 
 
-def map_query_results(query_results):
-    answers = [r['e1'] for r in query_results]
+def map_query_results(query_results, question_variable='e1'):
+    """
+    Extract the variable values from the query results and map them to canonical WebQuestions strings.
+
+    :param query_results: list of dictionaries returned by the sparql endpoint
+    :param question_variable: the variable to extract
+    :return: list of answers as entity labels or an original id if no canonical label was found.
+    >>> map_query_results(['Q76', 'Q235234'])
+    ['Barack Obama', 'Q235234']
+    """
+    answers = [r[question_variable] for r in query_results]
     answers = [e.lower() for a in answers for e in entity_map.get(a, [a])]
+    return answers
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
 
