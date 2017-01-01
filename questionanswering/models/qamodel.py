1198e57797079193ca741f6f6b5a799c7132a5a1
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2017-01-01 21:53:31 +0100

    fix epsilon bug

--- questionanswering/models/qamodel.py
+++ questionanswering/models/qamodel.py
@@ -167,7 +167,7 @@ class TwinsModel(KerasModel, metaclass=abc.ABCMeta):
         predictions = np.dot(sentence_embedding, np.swapaxes(edge_embeddings, 0, 1))
         if self._p.get("twin.similarity", "dot") == "cos":
             denominator = np.sqrt(np.sum(sentence_embedding*sentence_embedding) * np.sum(edge_embeddings*edge_embeddings, axis=-1))
-            denominator = np.maximum(denominator, keras.backend._EPSILON)
+            denominator = np.maximum(denominator, keras.backend.common._EPSILON)
             predictions /= denominator
 
         return np.argsort(predictions)[::-1]
