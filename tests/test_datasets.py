154dd70313c36329e7039bdfaa9151e4be14f54d
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2017-01-18 16:13:03 +0100

    fix tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,12 +1,20 @@
 import pytest
 import yaml
 import numpy as np
+import logging
 
 from questionanswering.datasets import webquestions_io
 
 with open("../questionanswering/default_config.yaml", 'r') as config_file:
     config = yaml.load(config_file.read())
-webquestions = webquestions_io.WebQuestions(config['webquestions'])
+
+logger = logging.getLogger(__name__)
+logger.setLevel(config['logger']['level'])
+ch = logging.StreamHandler()
+ch.setLevel(config['logger']['level'])
+logger.addHandler(ch)
+
+webquestions = webquestions_io.WebQuestions(config['webquestions'], logger=logger)
 
 
 def test_load_webquestions():
@@ -17,7 +25,10 @@ def test_load_webquestions():
 def test_access_sample():
     input_set, targets = webquestions.get_training_samples()
     assert len(input_set) == len(targets)
-    assert type(targets[0]) == np.int32
+    if config['webquestions'].get('target.dist'):
+        assert len(targets[0]) == config['webquestions'].get('max.negative.samples')
+    else:
+        assert type(targets[0]) == np.int32
     assert all(['edgeSet' in g for g in input_set[0]])
 
 
