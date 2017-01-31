a5996348e82177206a45bbd82d4942c23e1c0dbc
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2017-01-31 18:04:11 +0100

    bug fix

--- questionanswering/construction/staged_generation.py
+++ questionanswering/construction/staged_generation.py
@@ -479,7 +479,7 @@ def post_process_answers_given_graph(model_answers_labels, g):
     :param model_answers_labels: list of list of answers
     :param g: graph as a dictionary
     :return: list of list of answers
-    >>> post_process_answers_given_graph([['eng', 'english']], {'edgeSet':[{'kbID': 'P37v', 'rightKbID':'Q843'}]})
+    >>> post_process_answers_given_graph([['eng', 'english']], {'edgeSet':[{'kbID': 'P37v', 'rightkbID':'Q843'}]})
     [['eng', 'english', 'eng language', 'english language'], ['pakistani english', 'pakistani english language']]
     """
     relevant_edge = [e for e in g.get('edgeSet', []) if e.get("kbID", "")[:-1] == "P37"]
@@ -487,7 +487,7 @@ def post_process_answers_given_graph(model_answers_labels, g):
         for answer_set in model_answers_labels:
             if all('language' not in a.lower() for a in answer_set):
                 answer_set.extend([a + " language" for a in answer_set])
-            if 'english' in answer_set and relevant_edge[0].get('rightKbID'):
+            if 'english' in answer_set and relevant_edge[0].get('rightkbID'):
                 demonym = wdaccess.query_wikidata(wdaccess.demonym_query(relevant_edge[0].get('rightKbID')), starts_with="")
                 if demonym:
                     demonym = demonym[0]['labelright'].lower()
