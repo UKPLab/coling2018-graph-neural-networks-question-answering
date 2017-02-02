8bb710bb9ce6e4b89bbec1eeb551bba4bb5eab2a
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2017-02-02 23:34:59 +0100

    new ner model for stanford

--- questionanswering/stanford_tag_dataset.py
+++ questionanswering/stanford_tag_dataset.py
@@ -29,6 +29,6 @@ if __name__ == "__main__":
                                                   list(zip(*webquestions_utterances_nes[i]))[1],
                                                   list(zip(*webquestions_utterances_poss[i]))[1])) for i, tokens in enumerate(webquestions_utterances_tokens)]
 
-    with open(data_folder + "generated/webqsp.examples.test.utterances.tagged.json", "w") as out:
+    with open(data_folder + "generated/webquestions.examples.test.utterances.tagged.json", "w") as out:
         json.dump(webquestions_utterances_alltagged, out, indent=4)
     logger.debug("Saved")
