c390294dad8794cb732c22e4c589f1eb0d288c75
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2017-02-02 23:38:20 +0100

    new ner model for stanford

--- questionanswering/stanford_tag_dataset.py
+++ questionanswering/stanford_tag_dataset.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     tokenizer = nltk.tokenize.stanford.StanfordTokenizer(path_to_jar = "../resources/stanford-ner-2015-12-09/stanford-ner-3.6.0.jar")
     ne_tagger = nltk.tag.stanford.StanfordNERTagger("../resources/models-3.7.0/edu/stanford/nlp/models/ner/english.all.3class.caseless.distsim.crf.ser.gz", path_to_jar = "../resources/stanford-ner-2015-12-09/stanford-ner-3.6.0.jar")
-    pos_tagger = nltk.tag.stanford.StanfordPOSTagger("models-3.7.0/edu/stanford/nlp/models/pos-tagger/english-caseless-left3words-distsim.tagger", path_to_jar = "../resources/stanford-postagger-full-2015-12-09/stanford-postagger-3.6.0.jar")
+    pos_tagger = nltk.tag.stanford.StanfordPOSTagger("../resources/models-3.7.0/edu/stanford/nlp/models/pos-tagger/english-caseless-left3words-distsim.tagger", path_to_jar = "../resources/stanford-postagger-full-2015-12-09/stanford-postagger-3.6.0.jar")
     webquestions_utterances_tokens = tokenizer.tokenize_sents([q_obj['utterance'] for q_obj in webquestions])
     logging.debug('Tokenized')
     webquestions_utterances_nes = ne_tagger.tag_sents(webquestions_utterances_tokens)
