6570d38fbcfe104a4a76bce95b7a837f753d92a8
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2017-02-20 12:21:05 +0100

    caseless parameter to the stanford_tag_dataset script

--- questionanswering/stanford_tag_dataset.py
+++ questionanswering/stanford_tag_dataset.py
@@ -10,7 +10,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 @click.command()
 @click.argument('config_file_path', default="default_config.yaml")
-def tag_dataset(config_file_path):
+@click.option('-c', '--caseless', default=False, is_flag=True)
+def tag_dataset(config_file_path, caseless):
 
     config = utils.load_config(config_file_path)
 
@@ -20,11 +21,15 @@ def tag_dataset(config_file_path):
     with open(config['generation']['questions']) as f:
         webquestions = json.load(f)
     logging.debug('Loaded WebQuestions, size: {}'.format(len(webquestions)))
-
+    ner_model = "../resources/models-3.7.0/edu/stanford/nlp/models/ner/english.all.3class.distsim.crf.ser.gz"
+    pos_model = "../resources/models-3.7.0/edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger"
+    if caseless:
+        ner_model = "../resources/models-3.7.0/edu/stanford/nlp/models/ner/english.all.3class.caseless.distsim.crf.ser.gz"
+        pos_model = "../resources/models-3.7.0/edu/stanford/nlp/models/pos-tagger/english-caseless-left3words-distsim.tagger"
     tokenizer = nltk.tokenize.stanford.StanfordTokenizer(path_to_jar= "../resources/stanford-ner-2015-12-09/stanford-ner-3.6.0.jar")
-    ne_tagger = nltk.tag.stanford.StanfordNERTagger("../resources/models-3.7.0/edu/stanford/nlp/models/ner/english.all.3class.caseless.distsim.crf.ser.gz", path_to_jar="../resources/stanford-ner-2015-12-09/stanford-ner-3.6.0.jar")
-    pos_tagger = nltk.tag.stanford.StanfordPOSTagger("../resources/models-3.7.0/edu/stanford/nlp/models/pos-tagger/english-caseless-left3words-distsim.tagger", path_to_jar="../resources/stanford-postagger-full-2015-12-09/stanford-postagger-3.6.0.jar")
-    webquestions_utterances_tokens = tokenizer.tokenize_sents([q_obj.get('utterance', q_obj['question']) for q_obj in webquestions])
+    ne_tagger = nltk.tag.stanford.StanfordNERTagger(ner_model, path_to_jar="../resources/stanford-ner-2015-12-09/stanford-ner-3.6.0.jar")
+    pos_tagger = nltk.tag.stanford.StanfordPOSTagger(pos_model, path_to_jar="../resources/stanford-postagger-full-2015-12-09/stanford-postagger-3.6.0.jar")
+    webquestions_utterances_tokens = tokenizer.tokenize_sents([q_obj.get('utterance', q_obj.get('question')) for q_obj in webquestions])
     logging.debug('Tokenized')
     webquestions_utterances_nes = ne_tagger.tag_sents(webquestions_utterances_tokens)
     logging.debug('NE tags')
