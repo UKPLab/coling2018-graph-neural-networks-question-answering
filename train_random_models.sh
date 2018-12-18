eade5210560d62ae84816f2bb34f19fa82f738c0
Daniil Sorokin <daniil.sorokin@factmata.com>
2018-12-18 10:17:13 +0000

    Update the train script with a config parameter

--- train_random_models.sh
+++ train_random_models.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 for i in {1..30};
 do
-    GPUID=$1
+    GPUID=$2
+    CONFIGFILE=$1
     echo "### Training Phase ###"
-    MODELPATH="$(python -m questionanswering.train_model configs/train_gnn_full.yaml $i $GPUID | tail -n1)"
+    MODELPATH="$(python -m questionanswering.train_model $CONFIGFILE $i $GPUID | tail -n1)"
     echo "### Test Phase ###"
     python -m questionanswering.evaluate_on_test $MODELPATH configs/webqsp_eval_config.yaml $i $GPUID
 done
